a79dc10c52c992b3e457683a52ae688884338bbe
st-0403-jp <sato252011@gmail.com>
2016-04-16 14:11:06 +0900

    ちょっと微妙な実装なので廃止する

--- src/js/top.js
+++ src/js/top.js
@@ -17,29 +17,45 @@ page.top = (function () {
       });
       */
 
+      var $navLi = $('nav li');
       var $navList = $('nav a');
       var navIds = [];
       var id;
       var idTops = [];
+      /*
       Array.prototype.forEach.call($navList, function (data) {
         id = data.dataset.navId;
         idTops.push($('#' + id).offsetTop);
       });
+      */
 
-      console.log(idTops);
+      //console.log(idTops);
       
       var $article = $('article');
+      var offsetTopFunk;
+
       $article.addEventListener('scroll', function () {
-        Array.prototype.forEach.call($navList, function (data) {
-          id = data.dataset.navId;
-          //if (this.scroll) $('#' + id).offsetTop
+        if (offsetTopFunk) {
+          clearTimeout(offsetTopFunk);
+        } else {
+          offsetTopFunk = setTimeout(function () {
+            //timer = true;
+            Array.prototype.forEach.call($navList, function (data) {
+              id = data.dataset.navId;
+              idTops.push({top: $('#' + id).offsetTop, liClass: data.parentNode.classList});
+            });
+          }, 10);
+        }
+        var articleTop = this.scrollTop;
+        //console.log(articleTop);
+        idTops.forEach(function (tops) {
+          console.log(articleTop);
+          if (articleTop === tops.top) {
+            console.log('top位置');
+            tops.liClass.add('current');
+          }
         });
       });
-      /*
-      navIds.forEach(function (id) {
-        console.log(id);
-      });
-      */
     }
   };
 })();
